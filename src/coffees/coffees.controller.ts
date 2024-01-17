682ee25a9318bd96ea48faf785dbd3f35407eeb8
Devon Wolfkiel <devon.wolfkiel@hingehealth.com>
2024-01-16 16:51:51 -0800

    basic service lesson (plus accidental DTO)

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,37 +6,40 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { CoffeesService } from './coffees.service';
 
+// UP NEXT: user friendly error messages
 @Controller('coffees')
 export class CoffeesController {
-  @Get()
-  findAll() {
-    return 'This action returns all coffees';
-  }
+  // this.coffeesService gets created as a new instance of CoffeesService; it works just by passing it to the constructor because of the @Injectable decorator
+  constructor(private readonly coffeesService: CoffeesService) {}
 
-  @Get('flavors')
-  findAllFlavors() {
-    return 'This action returns all flavors';
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns coffee ${id}`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates coffee ${id} with ${JSON.stringify(body)}`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes coffee ${id}`;
+    return this.coffeesService.remove(id);
   }
 }
